4739a328d702c0ac7db5ad6f67de73c399794b02
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-07 20:08:28 +0100

    added header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,12 +6,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
-    <link rel="icon" href="{{ asset('images/adv.jpg') }}" type="image/ico" >
+    <link rel="icon" href="{{ Vite::asset('resources/img/favicon.ico') }}" type="image/ico" >
 
     <!-- Style -->
     @vite('resources/js/app.js')
 </head>
 <body>
+
+    {{-- Header --}}
+    <header>
+        <div class="container header-elements">
+            <figure>
+                <a href="#">
+                    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo DC Comics">
+                </a>
+            </figure>
+            <nav>
+                <ul class="header-list">
+                    <li class=""><a href="">Characters</a></li>
+                    <li class=""><a href="">Comics</a></li>
+                    <li class=""><a href="">Movies</a></li>
+                    <li class=""><a href="">Tv</a></li>
+                    <li class=""><a href="">Games</a></li>
+                    <li class=""><a href="">Collectibles</a></li>
+                    <li class=""><a href="">Videos</a></li>
+                    <li class=""><a href="">Fans</a></li>
+                    <li class=""><a href="">News</a></li>
+                    <li class=""><a href="">Shop</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
     
 </body>
 </html>
