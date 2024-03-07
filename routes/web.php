c7d2fe41714555fdc7abd350dd89b5bf47a1d409
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-07 21:05:50 +0100

    added new page

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
+
+Route::get('/characters', function () {
+    return view('characters');
+})->name('characters');
