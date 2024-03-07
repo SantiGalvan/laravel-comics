2491035ca22f6bff2158d60700f893891d80b86f
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-07 21:02:40 +0100

    created & added includes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,30 +14,13 @@
 <body>
 
     {{-- Header --}}
-    <header>
-        <div class="container header-elements">
-            <figure>
-                <a href="#">
-                    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo DC Comics">
-                </a>
-            </figure>
-            <nav>
-                <ul class="header-list">
-                    @foreach (config('header_menu') as $link)
-                    <li class="{{ Route::is($link['route_name']) ? 'current' : ''}}"><a href="">{{$link['text']}}</a></li>
-                    @endforeach
-
-                </ul>
-            </nav>
-        </div>
-    </header>
+    @include('includes.header')
 
     {{-- Main --}}
     <main>
 
         {{-- Section Jumbotron --}}
-        <section id="section-jumbotron">
-        </section>
+        <section id="section-jumbotron"></section>
 
         {{-- Section Content --}}
         <section id="section-content">
@@ -91,85 +74,7 @@
     </main>
 
     {{-- Footer --}}
-     <footer>
-        <div class="upper-footer">
-            <div class="container footer-container">
-                <nav>
-                    <div class="col">
-                        <h4>Dc comics</h4>
-                        <ul>
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Comics</a></li>
-                            <li><a href="#">Movies</a></li>
-                            <li><a href="#">TV</a></li>
-                            <li><a href="#">Games</a></li>
-                            <li><a href="#">News</a></li>
-                        </ul>
-                        <h4>Shop</h4>
-                        <ul>
-                            <li><a href="#">Shop DC</a></li>
-                            <li><a href="#">Shop DC Collectibles</a></li>
-                        </ul>
-                    </div>
-                    <div class="col">
-                        <h4>Dc</h4>
-                        <ul>
-                            <li><a href="#">Therms of use</a></li>
-                            <li><a href="#">Privacy policy (NEW)</a></li>
-                            <li><a href="#">Add Choise</a></li>
-                            <li><a href="#">Advertising</a></li>
-                            <li><a href="#">Jobs</a></li>
-                            <li><a href="#">Subscriptions</a></li>
-                            <li><a href="#">Talent Workshops</a></li>
-                            <li><a href="#">CPSC Certification</a></li>
-                            <li><a href="#">Ratings</a></li>
-                            <li><a href="#">Shop Help</a></li>
-                            <li><a href="#">Contact Us</a></li>
-                        </ul>
-                    </div>
-                    <div class="col">
-                        <h4>Dc comics</h4>
-                        <ul>
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Comics</a></li>
-                            <li><a href="#">Movies</a></li>
-                            <li><a href="#">TV</a></li>
-                            <li><a href="#">Games</a></li>
-                            <li><a href="#">News</a></li>
-                        </ul>
-                    </div>
-                </nav>
-                <div class="logo-bg"></div>
-            </div>
-        </div>
-        <div class="bottom-footer">
-            <div class="container footer-container">
-                <div class="actions">
-                    <button>Sign-up now!</button>
-                </div>
-                <div class="follow">
-                    <h2>Follow us</h2>
-                    <figure>
-                        <a href="https://www.facebook.com/?locale=it_IT" target="_blank">
-                            <img src="{{Vite::asset('resources/img/footer-facebook.png')}}" alt="Facebook">
-                        </a>
-                        <a href="https://twitter.com/?lang=it" target="_blank">
-                            <img src="{{Vite::asset('resources/img/footer-twitter.png')}}" alt="Twitter">
-                        </a>
-                        <a href="https://www.youtube.com/" target="_blank">
-                            <img src="{{Vite::asset('resources/img/footer-youtube.png')}}" alt="YouTube">
-                        </a>
-                        <a href="https://www.pinterest.it/" target="_blank">
-                            <img src="{{Vite::asset('resources/img/footer-pinterest.png')}}" alt="Pinterest">
-                        </a>
-                        <a href="https://www.pscp.tv/" target="_blank">
-                            <img src="{{Vite::asset('resources/img/footer-periscope.png')}}" alt="Periscope">
-                        </a>
-                    </figure>
-                </div>
-            </div>
-        </div>
-    </footer>
+    @include('includes.footer')
     
 </body>
 </html>
