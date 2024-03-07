3f899478a622c571fcb401e10536d6bd046e3395
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-07 21:26:25 +0100

    added elements & style

--- resources/views/characters.blade.php
+++ resources/views/characters.blade.php
@@ -3,5 +3,8 @@
 @section('title', 'Characters')
 
 @section('main-content')
-
+    {{-- Section Characters --}}
+    <section id="section-characters">
+        <h1>Characters</h1>
+    </section>
 @endsection
