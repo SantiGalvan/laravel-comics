90720e55bea2fa7426d695119531b9108713a45d
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-07 19:41:29 +0100

    added icon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Home</title>
+    <link rel="icon" href="{{ asset('images/adv.jpg') }}" type="image/ico" >
 
     <!-- Style -->
     @vite('resources/js/app.js')
