0e8545d2e7316e48237e5cccb754c7bf6ecd36b8
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-08 18:07:49 +0100

    added element & style

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -2,6 +2,13 @@
 
 @section('title', 'Comic')
 
+@section('cdns')
+
+{{-- FotAwesome --}}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+@endsection
+
 @section('main-content')
 
 {{-- Section View --}}
@@ -13,7 +20,19 @@
         <div class="comic-container">
             <div class="comic-info">
                 <h1>{{$comic['title']}}</h1>
-                <div class="comic-price"></div>
+                <div class="comic-price">
+                    <div class="price">
+                        <div class="price-info">
+                            <h4>U.S. Price: <span>{{$comic['price']}}</span></h4>
+                        </div>
+                        <div class="price-text">
+                            <h4>Available</h4>
+                        </div>
+                    </div>
+                    <div class="check">
+                        <p>Check availability <i class="fa-solid fa-sort-down" ></i></p>
+                    </div>
+                </div>
                 <div class="comic-description">
                     <p>{{$comic['description']}}</p>
                 </div>
